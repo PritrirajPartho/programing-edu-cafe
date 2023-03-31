928e61cd4849bd1d472c6ab3aff973b0ab8f663a
PritrirajPartho <dapartho5616@gmail.com>
2023-03-31 22:20:43 +0600

    six-added

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,8 @@ function App() {
         <h1>Hello React Assignment.......</h1>
         <h5>Tumse milna bath o karna. bada accha lag ta hai</h5>
         <h5>I am coming git</h5>
-        <h5>jibon bedona . kichu korte hoibe</h5>
+        <h5>jibon bedona . kichu korte hoibe. </h5>
+        <h6>ar ki chau tumi</h6>
     </div>
   )
 }
